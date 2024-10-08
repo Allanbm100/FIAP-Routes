c444cf01ef345695c6f1bfd92aca200b0e2e43b8
allanbm100 <allan.bm.100@gmail.com>
2024-10-07 22:19:36 -0300

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Menu } from "../components/menu/Menu";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Home</h1>
+            <Menu />
+        </main>
+    )
+}
